cb29c6ba2dff98d43991d5898f3da123f2d5ecf7
Safari7251 <abdullahmathhisafari@gmail.com>
2024-12-01 09:04:34 +0000

    changed Dbname

--- 22.11.2024/dbconf.php
+++ 22.11.2024/dbconf.php
@@ -3,7 +3,7 @@
 define('SERVERNAME', '127.0.0.1:3306');
 define('USERNAME', 'root');
 define('PASSWORD', 'mariadb');
-define('DBNAME', 'school');
+define('DBNAME', 'employee');
 try{
 	//connect with database
 $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
